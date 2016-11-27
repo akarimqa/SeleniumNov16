d116379ebbbf1621c4859852dbc5b92596fc7b39
akarimqa <akarimqa@gmail.com>
2016-11-27 11:45:28 -0500

    initial setup

---
+++ src/Lec3Nov27/prac1.java
@@ -0,0 +1,11 @@
+package Lec3Nov27;
+
+public class prac1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good morning class");
+
+	}
+
+}
